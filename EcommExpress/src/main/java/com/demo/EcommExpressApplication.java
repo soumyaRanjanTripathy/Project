7b49eb93366a6728e141da735a164439eac36ee3
soumya <pinkutripathy>
2024-07-30 12:34:35 +0530

    30/07/2024

---
+++ EcommExpress/src/main/java/com/demo/EcommExpressApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommExpressApplication.class, args);
+	}
+
+}
